a965470e46692373ed3c4280907f218c31bc4cdd
Lauren <laurenbongartz@laurens-macbook-pro.local>
2017-03-28 13:59:05 -0700

    With audio!

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var gridView: GridView!
+    @IBOutlet weak var wv: UIWebView!
+    
+    var playedVideo = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +26,19 @@ class ViewController: UIViewController {
         gridView.diedColor = UIColor.darkGray.withAlphaComponent(0.6)
         gridView.gridColor = UIColor.black
         gridView.gridWidth = 2.0
+    
     }
 
     @IBAction func stepAction(_ sender: Any) {
         gridView.grid = gridView.grid.next()
         gridView.setNeedsDisplay()
+        
+        if (playedVideo == 0) {
+             wv.loadRequest(URLRequest(url: URL(string: "https://www.youtube.com/watch?v=1g5sLxevtkM")!))
+            playedVideo = 1;
+        } else {
+            wv.isHidden = true;
+        }
     }
 
 }
